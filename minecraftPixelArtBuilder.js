250cb68a56ec89c886d3ff7f378b991ad6ad0247
DuoGetsRekt <50208407+DuoGetsRekt@users.noreply.github.com>
2019-10-22 14:22:12 +0100

    Add files via upload

--- minecraftPixelArtBuilder.js
+++ minecraftPixelArtBuilder.js
@@ -118,7 +118,7 @@ function displayPixelArt(colorDataSet, ratio) {
 
             colorDataSet[i].minecraftBlockAssigned = blockBoundaryData.blockName[j];
             console.log("bv: " + colorDataSet[i].minecraftBlockAssigned + " xIndex " + colorDataSet[i].xIndex + " yIndex " + colorDataSet[i].yIndex);
-            context.drawImage(blockImages[blockBoundaryData.blockName[j]].src,  0, 0, 16, 16, (colorDataSet[i].xIndex * 16) - 16, (colorDataSet[i].yIndex * 16) - 16, 16, 16);
+            context.drawImage(blockImages[blockBoundaryData.blockName[j]],  0, 0, 16, 16, (colorDataSet[i].xIndex * 16) - 16, (colorDataSet[i].yIndex * 16) - 16, 16, 16);
           }
 
         }
